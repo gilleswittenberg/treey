e251b53ef781acd3e8443f7f24e727e7efe8326f
Gilles Wittenberg <git@gilleswittenberg.com>
2019-07-26 18:02:50 +0200

    TODO

--- src/createUuid.ts
+++ src/createUuid.ts
@@ -1,6 +1,7 @@
 // @LINK: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 const createUUID = () : string => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c: string) => {
+    // @TODO: Use cryptographically secure RNG
     const r = Math.random() * 16 | 0
     const v = c == 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
