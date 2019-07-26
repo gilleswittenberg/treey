e154f0b7ff2d3010c1d2da3fc3bece10eec51bb5
Gilles Wittenberg <git@gilleswittenberg.com>
2019-07-26 17:58:06 +0200

    Multiformat hash

--- src/generateHash.ts
+++ src/generateHash.ts
@@ -1,5 +1,11 @@
 import { Hash } from "./types"
+import * as multiformat from "multihashes"
 
-const generateHash = (message: any) : Hash => ""
+const generateHash = (obj: Object) : Hash => {
+  const message = JSON.stringify(obj)
+  const buffer = new multiformat.Buffer(message, "hex")
+  const encoded = multiformat.encode(buffer, "sha2-256")
+  return encoded.toString()
+}
 
 export default generateHash
