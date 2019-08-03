1fa2920503a99b4ce0bac90b65dc62d8a476c03e
Gilles Wittenberg <git@gilleswittenberg.com>
2019-08-03 17:55:39 +0200

    Remove undefined types

--- src/crud.ts
+++ src/crud.ts
@@ -5,21 +5,21 @@ import createItem from "./createItem"
 import database from "./database/database"
 import toArray from "./utils/toArray"
 
-const create = async (isRoot = false) : Promise<Item | undefined> => {
+const create = async (isRoot = false) : Promise<Item> => {
   const itemEventCreate = createEvent(ItemEventType.Create)
   const item = createItem([itemEventCreate])
   return await database.create(item, isRoot)
 }
 
-const read = async (id: Id) : Promise<Item | undefined> => {
+const read = async (id: Id) : Promise<Item> => {
   return database.read(id)
 }
 
-const update = async (id: Id, events: ItemEvent | ItemEvents) : Promise<Item | undefined> => {
+const update = async (id: Id, events: ItemEvent | ItemEvents) : Promise<Item> => {
   return database.update(id, toArray(events))
 }
 
-const del = async (id: Id) : Promise<Item | undefined> => {
+const del = async (id: Id) : Promise<Item> => {
   return await database.del(id)
 }
 
