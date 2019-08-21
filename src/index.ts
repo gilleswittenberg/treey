53af28038562987f8d96decea63086d694b874f6
Gilles Wittenberg <git@gilleswittenberg.com>
2019-08-21 22:56:31 +0200

    Index, package.json TS settings

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as treey from "./treey"
+import * as crud from "./crud"
+import { parseFullName } from "./factories/createFullName"
+
+export default {
+  treey,
+  crud,
+  utils: { parseFullName }
+}
