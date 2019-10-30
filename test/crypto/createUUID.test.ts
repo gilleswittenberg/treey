35b893dfae2229937d3ae72de4ba8a923b5feedc
Gilles Wittenberg <mail@gilleswittenberg.com>
2019-10-30 22:41:49 +0100

    Remove createUUID

--- test/crypto/createUUID.test.ts
+++
@@ -1,9 +0,0 @@
-import createUUID from "../../src/crypto/createUUID"
-
-const hex = "[0-9a-f]"
-const uuidRegExp = new RegExp(`^${ hex }{8}-${ hex }{4}-${ hex }{4}-${ hex }{4}-${ hex }{12}$`, "i")
-
-test("createUUID", () => {
-  const uuid = createUUID()
-  expect(uuid).toMatch(uuidRegExp)
-})
