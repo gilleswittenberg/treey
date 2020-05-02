533a916f106211f98612b5003d60178e56325a1b
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-05-02 19:58:24 +0200

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,44 @@ import * as crud from "./crud"
 import createFullName, { parseFullName } from "./factories/createFullName"
 
 export default treey
-export type Treey = typeof treey
 export { crud }
 export { createFullName }
 export { parseFullName }
+
+export type Treey = typeof treey
+
+// @TODO: Remove duplication. Use types from declaration files
+export type Name = string
+export type FullName = string
+export type Protocol = string
+export type Id = {
+  name: Name
+  protocol?: Protocol
+}
+export type Ids = Id[]
+export type Data = any
+export type Index = number
+export type Schema = Record<string, unknown>
+export type State = {
+  readonly ids?: Ids
+  readonly schema?: Schema
+  readonly data?: Data
+  readonly relations?: Ids
+}
+export type TreeItem = {
+  readonly state: State
+  readonly events: any[]
+  readonly relations: TreeItems
+  readonly name: FullName
+  readonly isCyclic: boolean
+  readonly isKnown: boolean
+  readonly isDestroyed: boolean
+  readonly path?: Path
+}
+export type TreeItems = TreeItem[]
+export type ItemEvent = any
+export type Path = string
+export type Paths = Path[]
+export type TreeNode = {
+  readonly path?: Path
+}
