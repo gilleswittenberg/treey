a5a159899ca5fe3144b37376d6211c437ac56b40
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-05-02 18:13:32 +0200

    Types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import * as treey from "./treey"
 import * as crud from "./crud"
 import createFullName, { parseFullName } from "./factories/createFullName"
 
-export default {
-  treey,
-  crud,
-  utils: {
-    createFullName,
-    parseFullName
-  }
-}
+export default treey
+export { crud }
+export { createFullName }
+export { parseFullName }
