befccd1f076eb0e332d26f35d013fba85f02464e
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-05-02 18:31:04 +0200

    Capital type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as treey from "./treey"
+import * as Treey from "./treey"
 import * as crud from "./crud"
 import createFullName, { parseFullName } from "./factories/createFullName"
 
-export default treey
+export default Treey
 export { crud }
 export { createFullName }
 export { parseFullName }
